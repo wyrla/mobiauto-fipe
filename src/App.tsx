53c91e3436bb2d8ec6a41974fb834da4de7ca9e0
wyrla <111397148+wyrla@users.noreply.github.com>
2025-01-27 19:54:38 -0300

    feat: setup home page and react router config

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
+import { ThemeProvider } from "@emotion/react"
+import { createBrowserRouter, RouterProvider } from "react-router"
+import { theme } from "./lib/mui/theme"
+import { HomePage } from "./pages/Home"
+
+const router = createBrowserRouter([
+  { path: '/', Component: HomePage }
+])
+
 function App() {
 
   return (
-    <>re
-    
-    </>
+    <ThemeProvider theme={theme}>
+      <RouterProvider router={router} />
+    </ThemeProvider>
   )
 }
 
