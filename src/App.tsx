68448cc16b3be3913ed673062b77839dc3046821
wyrla <111397148+wyrla@users.noreply.github.com>
2025-01-27 20:10:48 -0300

    feat: add css reset aka mui's css base line

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { ThemeProvider } from "@emotion/react"
 import { createBrowserRouter, RouterProvider } from "react-router"
 import { theme } from "./lib/mui/theme"
 import { HomePage } from "./pages/Home"
+import { CssBaseline } from "@mui/material"
 
 const router = createBrowserRouter([
   { path: '/', Component: HomePage }
@@ -11,6 +12,7 @@ function App() {
 
   return (
     <ThemeProvider theme={theme}>
+      <CssBaseline />
       <RouterProvider router={router} />
     </ThemeProvider>
   )
