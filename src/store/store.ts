310a1c3707d3265d8a262aed62771c882345fb23
wyrla <111397148+wyrla@users.noreply.github.com>
2025-01-28 15:08:16 -0300

    feat: add slices into store

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,7 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit'
+import formReducer from './slices/formSlice'
+import listReducer from './slices/listSlice'
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    form: formReducer,
+    list: listReducer,
+  },
 })
 
 export type RootState = ReturnType<typeof store.getState>
