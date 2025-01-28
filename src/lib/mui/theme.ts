6da10eb662433c00806204917133e055c31383e3
wyrla <111397148+wyrla@users.noreply.github.com>
2025-01-28 11:16:52 -0300

    feat: add locale as ptBr

--- src/lib/mui/theme.ts
+++ src/lib/mui/theme.ts
@@ -1,46 +1,50 @@
 import { createTheme } from "@mui/material";
+import { ptBR } from "@mui/material/locale";
 
-export const theme = createTheme({
-  palette: {
-    background: {
-      default: "#F9F6FC"
-    }
-  },
-  components: {
-    MuiButton: {
-      styleOverrides: {
-        root: {
-          background: "#5D00BF"
-        }
-      }
-    },
-    MuiFormControl: {
-      styleOverrides: {
-        root: {
-          margin: ".4rem 0"
-        }
-      }
-    },
-    MuiChip: {
-      styleOverrides: {
-        root: {
-          background: "#00A38C",
-          color: "#fff",
-          fontSize: "1rem",
-        }
+export const theme = createTheme(
+  {
+    palette: {
+      background: {
+        default: "#F9F6FC",
       },
-    }
+    },
+    components: {
+      MuiButton: {
+        styleOverrides: {
+          root: {
+            background: "#5D00BF",
+          },
+        },
+      },
+      MuiFormControl: {
+        styleOverrides: {
+          root: {
+            margin: ".4rem 0",
+          },
+        },
+      },
+      MuiChip: {
+        styleOverrides: {
+          root: {
+            background: "#00A38C",
+            color: "#fff",
+            fontSize: "1rem",
+          },
+        },
+      },
+    },
+    typography: {
+      fontFamily: "Roboto",
+      h1: {
+        fontSize: "2.5rem",
+      },
+      h2: {
+        fontSize: "0.875",
+      },
+      caption: {
+        color: "#747474",
+      },
+    },
   },
-  typography:{
-    fontFamily: "Roboto",
-    h1: {
-      fontSize: "2.5rem",
-    },
-    h2: {
-      fontSize: "0.875",
-    },
-    caption: {
-      color: "#747474"
-    }
-  }
-})
+  ptBR
+);
