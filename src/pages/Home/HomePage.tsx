01d50e6dc25b7b29e1778944497851da568c3ae3
wyrla <111397148+wyrla@users.noreply.github.com>
2025-01-27 23:18:04 -0300

    feat: add model field and submit handle

--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -1,24 +1,29 @@
 import { useEffect, useState } from "react";
-import { Autocomplete, FormControl, Text } from "../../components";
+import { Autocomplete, Button, FormControl, Text } from "../../components";
 import { CustomCard, HomePageWrapper } from "./HomePage.styles";
-import { FipeItem, getBrands, getModels } from "../../api/fipe";
-
-
+import { FipeItem, getBrands, getModels, getModelYear } from "../../api/fipe";
 
 export const HomePage: React.FC = () => {
-  const [formData, setFormData] = useState<Record<'model' | 'brand' | 'year', FipeItem | null>>({
+  const [formData, setFormData] = useState<
+    Record<"model" | "brand" | "year", FipeItem | null>
+  >({
     brand: null,
     model: null,
     year: null,
-  })
+  });
   const [brands, setBrands] = useState<FipeItem[]>([]);
   const [models, setModels] = useState<FipeItem[]>([]);
+  const [yearList, setYearList] = useState<FipeItem[]>([]);
   useEffect(() => {
     fetchItems();
   }, []);
   useEffect(() => {
-    if(formData.brand) fetchModels(formData.brand!.codigo);
+    if (formData.brand) fetchModels(formData.brand!.codigo);
   }, [formData.brand]);
+  useEffect(() => {
+    if (formData.model)
+      fetchModelYear(formData.brand!.codigo, formData.model!.codigo);
+  }, [formData.model]);
 
   const fetchModels = async (brandId: string) => {
     const models = await getModels(brandId);
@@ -29,28 +34,60 @@ export const HomePage: React.FC = () => {
     const brands = await getBrands();
     setBrands(brands);
   };
+  const fetchModelYear = async (brandId: string, model: string) => {
+    const brands = await getModelYear({
+      brandCode: brandId,
+      modelCode: model,
+    });
+    setYearList(brands);
+  };
+
+  const handleFormSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+    console.log(formData);
+  };
 
   return (
     <HomePageWrapper>
       <Text variant="h1">Tabela Fipe</Text>
       <Text variant="h2">Consulte o valor de um veículo de forma gratuita</Text>
       <CustomCard>
-        <form>
+        <form onSubmit={handleFormSubmit}>
           <FormControl fullWidth>
-            <Autocomplete 
-            label="Marca"
-            value={formData.brand}
-            onChange={(_, value) => setFormData({...formData, brand: value})}
-            options={brands} />
+            <Autocomplete
+              label="Marca"
+              value={formData.brand}
+              onChange={(_, value) =>
+                setFormData({  brand: value, model: null, year: null })
+              }
+              options={brands}
+            />
           </FormControl>
           <FormControl fullWidth>
-            <Autocomplete 
-            label="Modelo"
-            disabled={!formData.brand}
-            value={formData.model}
-            onChange={(_, value) => setFormData({...formData, model: value})}
-            options={models} />
+            <Autocomplete
+              label="Modelo"
+              disabled={!formData.brand}
+              value={formData.model}
+              onChange={(_, value) =>
+                setFormData({ ...formData, model: value })
+              }
+              options={models}
+            />
           </FormControl>
+          {formData.model && (
+            <FormControl fullWidth>
+              <Autocomplete
+                label="Ano"
+                disabled={!formData.model}
+                value={formData.year}
+                onChange={(_, value) =>
+                  setFormData({ ...formData, year: value })
+                }
+                options={yearList}
+              />
+            </FormControl>
+          )}
+          <Button type="submit" variant="contained" >Consultar preço</Button>
         </form>
       </CustomCard>
     </HomePageWrapper>
