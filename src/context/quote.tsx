28606eb4542759d8029bcb6eb0c88a17806583c0
wyrla <111397148+wyrla@users.noreply.github.com>
2025-01-28 08:38:21 -0300

    refactor: add state logic into quote context

--- src/context/quote.tsx
+++ src/context/quote.tsx
@@ -3,28 +3,23 @@
 import { createContext, useContext, useMemo, useReducer } from "react";
 import { FipeItem } from "../api/fipe";
 
-type QuoteContextActions = {
-  lists: {
-    brands: FipeItem[];
-    models: FipeItem[];
-    year: FipeItem[];
-  };
+const initialState: State = {
+  form: {
+    brand: null,
+    model: null,
+    year: null,
+  },
+  lists: { brands: [], models: [], year: [] },
 };
 
-const QuoteContext = createContext<QuoteContextActions>({
-  lists: {
-    brands: [],
-    models: [],
-    year: [],
-  },
-});
+const QuoteContext = createContext<State>(initialState);
 
 const DispatchContext = createContext<
   React.Dispatch<DispatchAction> | undefined
 >(undefined);
 export const useDispatch = () => useContext(DispatchContext);
 
-export const useQuoteContext: () => QuoteContextActions = () => {
+export const useQuoteContext: () => State = () => {
   const context = useContext(QuoteContext);
 
   if (!context) {
@@ -36,20 +31,14 @@ export const useQuoteContext: () => QuoteContextActions = () => {
   return context;
 };
 
-const initialState: Lists = {
-  brands: [],
-  models: [],
-  year: [],
-};
-
 export const QuoteProvider = ({ children }) => {
-  const [lists, dispatch] = useReducer(listsReducer, initialState);
+  const [state, dispatch] = useReducer(listsReducer, initialState);
 
   const value = useMemo(
     () => ({
-      lists,
+      ...state,
     }),
-    [lists]
+    [state]
   );
   return (
     <QuoteContext.Provider value={value}>
@@ -60,34 +49,47 @@ export const QuoteProvider = ({ children }) => {
   );
 };
 
-// type ListsReducer = {lists
+type State = {
+  form: Record<"model" | "brand" | "year", FipeItem | null>;
+  lists: Lists;
+};
+
 type Lists = { brands: FipeItem[]; models: FipeItem[]; year: FipeItem[] };
 type DispatchAction = {
   type: "add_brands" | "add_models" | "add_models_by_year";
   payload: FipeItem[];
 };
 
-const listsReducer = (lists: Lists, action: DispatchAction): Lists => {
+const listsReducer = (state: State, action: DispatchAction): State => {
   switch (action.type) {
     case "add_brands": {
       return {
-        ...lists,
-        brands: action.payload,
+        ...state,
+        lists: {
+          ...state.lists,
+          brands: action.payload,
+        },
       };
     }
     case "add_models": {
       return {
-        ...lists,
-        models: action.payload,
+        ...state,
+        lists: {
+          ...state.lists,
+          models: action.payload,
+        },
       };
     }
     case "add_models_by_year": {
       return {
-        ...lists,
-        year: action.payload,
+        ...state,
+        lists: {
+          ...state.lists,
+          year: action.payload,
+        },
       };
     }
     default:
-      return lists;
+      return state;
   }
 };
