5e3a9c192ef351b39567417ee7f3de97234ea52e
wyrla <111397148+wyrla@users.noreply.github.com>
2025-01-28 16:14:41 -0300

    feat: api into store

--- src/store/store.ts
+++ src/store/store.ts
@@ -2,12 +2,16 @@ import { configureStore } from '@reduxjs/toolkit'
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux'
 import formReducer from './slices/formSlice'
 import listReducer from './slices/listSlice'
+import { api } from '../api/fipe'
 
 export const store = configureStore({
   reducer: {
     form: formReducer,
     list: listReducer,
+    [api.reducerPath]: api.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(api.middleware),
 })
 
 export type RootState = ReturnType<typeof store.getState>
