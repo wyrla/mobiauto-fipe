8d7c77fa9c020e78d348b4db5661927b715ed1bb
wyrla <111397148+wyrla@users.noreply.github.com>
2025-01-28 10:44:06 -0300

    refactor: setup theme customization

--- src/lib/mui/theme.ts
+++ src/lib/mui/theme.ts
@@ -3,7 +3,44 @@ import { createTheme } from "@mui/material";
 export const theme = createTheme({
   palette: {
     background: {
-      default: "#F6F9FC"
+      default: "#F9F6FC"
+    }
+  },
+  components: {
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          background: "#5D00BF"
+        }
+      }
+    },
+    MuiFormControl: {
+      styleOverrides: {
+        root: {
+          margin: ".4rem 0"
+        }
+      }
+    },
+    MuiChip: {
+      styleOverrides: {
+        root: {
+          background: "#00A38C",
+          color: "#fff",
+          fontSize: "1rem",
+        }
+      },
+    }
+  },
+  typography:{
+    fontFamily: "Roboto",
+    h1: {
+      fontSize: "2.5rem",
+    },
+    h2: {
+      fontSize: "0.875",
+    },
+    caption: {
+      color: "#747474"
     }
   }
 })
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import "@fontsource/roboto"; 
+import "@fontsource/roboto";
+import "@fontsource/roboto/700.css"; 
 import './index.css'
 import App from './App.tsx'
 
